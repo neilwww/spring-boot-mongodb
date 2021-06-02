bac9d168848779aa9873339455b25cb4ac2da479
Mateus Kroeber <mateusksodre@yahoo.com.br>
2021-06-01 22:00:22 -0300

    Query with many criteria

--- src/main/java/com/nelsonenterprises/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/nelsonenterprises/springbootmongodb/repository/PostRepository.java
@@ -1,14 +1,22 @@
 package com.nelsonenterprises.springbootmongodb.repository;
 
 import com.nelsonenterprises.springbootmongodb.domain.Post;
-import com.nelsonenterprises.springbootmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{ 'title' : {$regex: ?0, $options: '<i>'} }")
+    List<Post> searchTitle(String next);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
+
 }
--- src/main/java/com/nelsonenterprises/springbootmongodb/services/PostService.java
+++ src/main/java/com/nelsonenterprises/springbootmongodb/services/PostService.java
@@ -6,6 +6,7 @@ import com.nelsonenterprises.springbootmongodb.services.exceptions.ObjectNotFoun
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,7 +24,12 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text){
-        return repo.findByTitleContainingIgnoreCase(text);
+        return repo.searchTitle(text);
     }
 
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
+    }
 }
